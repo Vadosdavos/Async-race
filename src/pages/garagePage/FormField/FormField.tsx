52838b1b4b3e2c032fa38d9c9c5c7c04061fad08
Vadosdavos <bezbvap@gmail.com>
2022-02-14 19:16:28 +0200

    feat: add clearing inputs after create car

--- src/pages/garagePage/FormField/FormField.tsx
+++ src/pages/garagePage/FormField/FormField.tsx
@@ -1,21 +1,39 @@
-import { SyntheticEvent } from 'react';
+import { SyntheticEvent, useRef } from 'react';
 import styles from './FormField.styles.css';
 
 type FormProps = {
   type: string;
   handleTextInput: (event: SyntheticEvent) => void;
   handleColorInput: (event: SyntheticEvent) => void;
-  handleClick: () => void
+  handleClick: () => void;
 };
 
-export const FormField = ({ type, handleTextInput, handleColorInput, handleClick }: FormProps) => {
+export const FormField = ({ type, handleTextInput, handleColorInput, handleClick }: FormProps): JSX.Element => {
+  const nameRef = useRef(null);
+  const colorRef = useRef(null);
+  const clearInput = (): void => {
+    if (nameRef.current && colorRef.current) {
+      const nameInput = nameRef.current as HTMLInputElement;
+      const colorInput = colorRef.current as HTMLInputElement;
+      nameInput.value = '';
+      colorInput.value = '#000000';
+    }
+  };
 
   return (
     <>
       <div className={styles.formField}>
-        <input type='text' className={`${styles.textInput} ${styles[type]}`} onBlur={handleTextInput} />
-        <input type='color' className='colorInput' onBlur={handleColorInput} />
-        <button className={styles.subButton} onClick={handleClick}>{type}</button>
+        <input type='text' ref={nameRef} className={`${styles.textInput} ${styles[type]}`} onBlur={handleTextInput} />
+        <input type='color' ref={colorRef} className='colorInput' onBlur={handleColorInput} />
+        <button
+          className={styles.subButton}
+          onClick={() => {
+            handleClick();
+            clearInput();
+          }}
+        >
+          {type}
+        </button>
       </div>
     </>
   );
