13d1089b5debb08e807f22e81828b20ef4a4469b
Vadosdavos <bezbvap@gmail.com>
2022-02-17 17:00:20 +0200

    feat: add stop button

--- src/pages/garagePage/Track/Track.tsx
+++ src/pages/garagePage/Track/Track.tsx
@@ -1,5 +1,5 @@
-import { useRef } from 'react';
-import { driveMode, ICar, startEngine } from '../../../api/api';
+import { useRef, useState } from 'react';
+import { driveMode, ICar, startEngine, stopEngine } from '../../../api/api';
 import styles from './Track.styles.css';
 import { CarImg } from '../../../components/CarImg/CatImg';
 
@@ -22,6 +22,7 @@ export const Track = ({ name, color, id, onDelete, onSelect }: TrackProps): JSX.
   const carRef = useRef<HTMLInputElement>(null);
   const raceDistance = document.documentElement.clientWidth - CAR_MARGIN;
   const obj = { success: true };
+  const [isCarStarted, setIsCarStarted] = useState(false);
 
   const animateCar = ({ duration, draw }: IAnimate) => {
     let start = performance.now();
@@ -51,12 +52,20 @@ export const Track = ({ name, color, id, onDelete, onSelect }: TrackProps): JSX.
   };
 
   const handleStartClick = () => {
+    setIsCarStarted(true);
     const res = startEngine(id);
     res.then(({ distance, velocity }: { distance: number; velocity: number }) => {
       driveMode(id).then((res: { success: boolean }) => (obj.success = res.success));
       animateCar({ duration: distance / velocity, draw: drawCar });
     });
   };
+
+  const handleStopClick = () => {
+    setIsCarStarted(false);
+    const target = carRef.current;
+    stopEngine(id).then(() => (target ? (target.style.transform = `translateX(0)`) : null));
+  };
+
   return (
     <>
       <section>
@@ -71,10 +80,18 @@ export const Track = ({ name, color, id, onDelete, onSelect }: TrackProps): JSX.
             <div className={styles.name}>{name}</div>
           </div>
           <div className={styles.road}>
-            <button className={styles.startBtn} onClick={handleStartClick}>
+            <button
+              className={`${styles.startBtn} ${isCarStarted && styles.inactiveBtn}`}
+              onClick={handleStartClick}
+              disabled={isCarStarted}
+            >
               A
             </button>
-            <button className={`${styles.stopBtn} ${styles.inactiveBtn}`} disabled>
+            <button
+              className={`${styles.stopBtn} ${!isCarStarted && styles.inactiveBtn}`}
+              onClick={handleStopClick}
+              disabled={!isCarStarted}
+            >
               B
             </button>
             <div ref={carRef} className={styles.car}>
